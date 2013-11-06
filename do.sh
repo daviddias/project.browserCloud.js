3044ce04c1549b1df2695daa3ff18ba8f7a8e38b
David Dias <daviddias.p@gmail.com>
2013-11-06 20:52:07 +0000

    structuring, structuring, structuring

--- do.sh
+++ do.sh
@@ -1,3 +1,8 @@
+#How to?
+# Run once with /bibliography not commented in the individual section, then run again after commenting those lines
+
 pdflatex index
 bibtex index
 pdflatex index
+pdflatex index
+
